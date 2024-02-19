690c2512cfc05640f6bb835f26457f3881ee8653
dariomatias-dev <matiasdario75@gmail.com>
2024-02-19 14:57:20 -0300

    feat: created tests for the "Max" validator and improvements to the validator
    - Tests created for the "Max" validator that checks if the value is greater than a certain defined value.
    - Improved "Max" validator, allowing numbers of type int and float64 to be sent to the "value" and "max" values ​​of the validator, carrying out a split check to ensure that they will be correctly compared.

--- max_validator.go
+++ max_validator.go
@@ -15,15 +15,38 @@ func Max(
 
 	return func(value interface{}) (*string, bool) {
 		val, _ := value.(string)
-		if value.(float64) > max.(float64) {
-			if message == "" {
-				message = fmt.Sprintf(
-					"The maximum value is %v, but it received %d.",
-					max,
-					len(val),
-				)
+
+		if message == "" {
+			message = fmt.Sprintf(
+				"The maximum value is %v, but it received %d.",
+				max,
+				len(val),
+			)
+		}
+
+		switch value := value.(type) {
+		case int:
+			switch max := max.(type) {
+			case int:
+				if value > max {
+					return &message, false
+				}
+			case float64:
+				if float64(value) > max {
+					return &message, false
+				}
 			}
-			return &message, false
+		case float64:
+			switch max := max.(type) {
+			case int:
+				if value > float64(max) {
+					return &message, false
+				}
+			case float64:
+				if value > max {
+					return &message, false
+				}
+			}
 		}
 
 		return nil, false
---
+++ max_validator_test.go
@@ -0,0 +1,78 @@
+package validators
+
+import "testing"
+
+func TestMax(t *testing.T) {
+	customErrorMessage := "error"
+	errorMessage, stopLoop := Max(1)(2)
+	if errorMessage == nil || stopLoop {
+		t.Errorf(
+			"Max(1)(2) = %v, %t; expected: \"[error message]\", false",
+			GetValueFromErrorMessage(errorMessage),
+			stopLoop,
+		)
+	}
+
+	errorMessage, stopLoop = Max(1, customErrorMessage)(2)
+	if errorMessage == nil || stopLoop {
+		t.Errorf(
+			"Max(1, \"error\")(2) = %v, %t; expected: \"error\", false",
+			GetValueFromErrorMessage(errorMessage),
+			stopLoop,
+		)
+	}
+
+	errorMessage, stopLoop = Max(1.1)(1.2)
+	if errorMessage == nil || stopLoop {
+		t.Errorf(
+			"Max(1.1)(1.2) = %v, %t; expected: \"[error message]\", false",
+			GetValueFromErrorMessage(errorMessage),
+			stopLoop,
+		)
+	}
+
+	errorMessage, stopLoop = Max(1.1, customErrorMessage)(1.2)
+	if errorMessage == nil || stopLoop {
+		t.Errorf(
+			"Max(1.1, \"error\")(1.2) = %v, %t; expected: \"error\", false",
+			GetValueFromErrorMessage(errorMessage),
+			stopLoop,
+		)
+	}
+
+	errorMessage, stopLoop = Max(2)(1)
+	if errorMessage != nil || stopLoop {
+		t.Errorf(
+			"Max(2)(1) = %v, %t; expected: nil, false",
+			GetValueFromErrorMessage(errorMessage),
+			stopLoop,
+		)
+	}
+
+	errorMessage, stopLoop = Max(2, customErrorMessage)(1)
+	if errorMessage != nil || stopLoop {
+		t.Errorf(
+			"Max(2, \"error\")(1) = %v, %t; expected: nil, false",
+			GetValueFromErrorMessage(errorMessage),
+			stopLoop,
+		)
+	}
+
+	errorMessage, stopLoop = Max(2.1)(1.1)
+	if errorMessage != nil || stopLoop {
+		t.Errorf(
+			"Max(2.1)(1.1) = %v, %t; expected: nil, false",
+			GetValueFromErrorMessage(errorMessage),
+			stopLoop,
+		)
+	}
+
+	errorMessage, stopLoop = Max(2.1, customErrorMessage)(1.1)
+	if errorMessage != nil || stopLoop {
+		t.Errorf(
+			"Max(2.1, \"error\")(1.1) = %v, %t; expected: nil, false",
+			GetValueFromErrorMessage(errorMessage),
+			stopLoop,
+		)
+	}
+}
