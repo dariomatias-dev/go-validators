31842c2e76466a5c52a80bfd38d53592f2b1f8b8
dariomatias-dev <matiasdario75@gmail.com>
2024-02-19 15:19:44 -0300

    test: created tests for the "MinLength" and "MaxLength" validators
    - Tests created for the "MinLength" validator that verify if the length of the value is less than a certain defined value.
    - Tests created for the "MaxLength" validator that verify if the length of the value is greater than a certain defined value.

---
+++ max_length_validator_test.go
@@ -0,0 +1,42 @@
+package validators
+
+import "testing"
+
+func TestMaxLength(t *testing.T) {
+	customErrorMessage := "error"
+	errorMessage, stopLoop := MaxLength(5)("aAbBcC")
+	if errorMessage == nil || stopLoop {
+		t.Errorf(
+			"MaxLength(5)(\"aAbBcC\") = %v, %t; expected: \"[error message]\", false",
+			GetValueFromErrorMessage(errorMessage),
+			stopLoop,
+		)
+	}
+
+	errorMessage, stopLoop = MaxLength(5, customErrorMessage)("aAbBcC")
+	if errorMessage == nil || stopLoop {
+		t.Errorf(
+			"MaxLength(5, \"error\")(\"aAbBcC\") = %v, %t; expected: \"error\", false",
+			GetValueFromErrorMessage(errorMessage),
+			stopLoop,
+		)
+	}
+
+	errorMessage, stopLoop = MaxLength(5)("aA")
+	if errorMessage != nil || stopLoop {
+		t.Errorf(
+			"MaxLength(5)(\"aA\") = %v, %t; expected: \"[error message]\", false",
+			GetValueFromErrorMessage(errorMessage),
+			stopLoop,
+		)
+	}
+
+	errorMessage, stopLoop = MaxLength(5, customErrorMessage)("aA")
+	if errorMessage != nil || stopLoop {
+		t.Errorf(
+			"MaxLength(5, \"error\")(\"aA\") = %v, %t; expected: \"error\", false",
+			GetValueFromErrorMessage(errorMessage),
+			stopLoop,
+		)
+	}
+}
