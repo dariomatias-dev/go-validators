15a93003047479e9a2907c99f6495305b7eb7025
dariomatias-dev <matiasdario75@gmail.com>
2024-02-20 09:39:12 -0300

    test: created tests for the Regex validator
    - Tests created for the Regex validator, aiming to ensure it is functioning correctly.

---
+++ regex_validator_test.go
@@ -0,0 +1,60 @@
+package validators
+
+import "testing"
+
+func TestRegex(t *testing.T) {
+	numbersPattern := "^[0-9]+$"
+	errorMessage, stopLoop = Regex(
+		numbersPattern,
+		"Invalid value",
+	)("aA01")
+	if errorMessage == nil || stopLoop {
+		newError(t, `
+Regex(
+	"^[0-9]+$",
+	"Invalid value",
+)("aA01") = %s, %t; expected: "[error message]", false
+		`)
+	}
+
+	errorMessage, stopLoop = Regex(
+		numbersPattern,
+		"Invalid value",
+	)("0123")
+	if errorMessage != nil || stopLoop {
+		newError(t, `
+Regex(
+	"^[0-9]+$",
+	"Invalid value",
+)("0123") = %s, %t; expected: nil, false
+		`)
+	}
+
+	lettersPattern := "^[a-zA-Z]+$"
+	errorMessage, stopLoop = Regex(
+		lettersPattern,
+		"Invalid value",
+	)("aA01")
+	if errorMessage == nil || stopLoop {
+		newError(t, `
+Regex(
+	"^[a-zA-Z]+$",
+	"Invalid value",
+)("aA01") = %s, %t; expected: "[error message]", false
+		`)
+	}
+
+	lettersPattern = "^[a-zA-Z]+$"
+	errorMessage, stopLoop = Regex(
+		lettersPattern,
+		"Invalid value",
+	)("aAbB")
+	if errorMessage != nil || stopLoop {
+		newError(t, `
+Regex(
+	"^[a-zA-Z]+$",
+	"Invalid value",
+)("aAbB") = %s, %t; expected: nil, false
+		`)
+	}
+}
