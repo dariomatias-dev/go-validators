ae316c718653d80aae66e4c147052ec26a7fedb1
dariomatias-dev <matiasdario75@gmail.com>
2024-02-21 17:59:01 -0300

    feat: Created tests for interface and int arrays in the "IsArray" validator.
    - Added tests to verify if the validation of interface arrays in the "IsArray" validator is functioning correctly.
    - Added tests to verify if the validation of int arrays in the "IsArray" validator is functioning correctly.

--- is_array_validator_test.go
+++ is_array_validator_test.go
@@ -5,6 +5,79 @@ import "testing"
 func TestIsArray(t *testing.T) {
 	customErrorMessage := "error"
 
+	/// Interface Array
+	// Errors
+	errorMessage, stopLoop = IsArray(
+		InterfaceArray,
+		[]Validator{},
+	)([]string{})
+
+	if errorMessage == nil || !stopLoop {
+		t.Errorf(`
+errorMessage, stopLoop = IsArray(
+	InterfaceArray,
+	[]Validator{},
+)([]string{}) = %v, %t; expected: "[error message]", true
+		`,
+			getArgs()...,
+		)
+	}
+
+	errorMessage, stopLoop = IsArray(
+		InterfaceArray,
+		[]Validator{},
+		customErrorMessage,
+	)([]string{})
+
+	if errorMessage == nil || *errorMessage != customErrorMessage || !stopLoop {
+		t.Errorf(`
+errorMessage, stopLoop = IsArray(
+	InterfaceArray,
+	[]Validator{},
+	"error",
+)([]string{}) = %v, %t; expected: "error", true
+		`,
+			getArgs()...,
+		)
+	}
+
+	// Successes
+	errorMessage, stopLoop = IsArray(
+		InterfaceArray,
+		[]Validator{},
+	)([]interface{}{})
+
+	if errorMessage != nil || stopLoop {
+		t.Errorf(
+			`
+errorMessage, stopLoop = IsArray(
+	InterfaceArray,
+	[]Validator{},
+)([]interface{}{}) = %v, %t; expected: nil, false
+			`,
+			getArgs()...,
+		)
+	}
+
+	errorMessage, stopLoop = IsArray(
+		InterfaceArray,
+		[]Validator{},
+		customErrorMessage,
+	)([]interface{}{})
+
+	if errorMessage != nil || stopLoop {
+		t.Errorf(
+			`
+errorMessage, stopLoop = IsArray(
+	InterfaceArray,
+	[]Validator{},
+	"error",
+)([]interface{}{}) = %v, %t; expected: nil, false
+			`,
+			getArgs()...,
+		)
+	}
+
 	/// String Array
 	// Errors
 	errorMessage, stopLoop = IsArray(
@@ -73,6 +146,79 @@ errorMessage, stopLoop = IsArray(
 	[]Validator{},
 	"error",
 )([]string{}) = %v, %t; expected: nil, false
+			`,
+			getArgs()...,
+		)
+	}
+
+	/// Int Array
+	// Errors
+	errorMessage, stopLoop = IsArray(
+		IntArray,
+		[]Validator{},
+	)([]interface{}{})
+
+	if errorMessage == nil || !stopLoop {
+		t.Errorf(`
+errorMessage, stopLoop = IsArray(
+	IntArray,
+	[]Validator{},
+)([]interface{}{}) = %v, %t; expected: "[error message]", true
+		`,
+			getArgs()...,
+		)
+	}
+
+	errorMessage, stopLoop = IsArray(
+		IntArray,
+		[]Validator{},
+		customErrorMessage,
+	)([]interface{}{})
+
+	if errorMessage == nil || *errorMessage != customErrorMessage || !stopLoop {
+		t.Errorf(`
+errorMessage, stopLoop = IsArray(
+	IntArray,
+	[]Validator{},
+	"error",
+)([]interface{}{}) = %v, %t; expected: "error", true
+		`,
+			getArgs()...,
+		)
+	}
+
+	// Successes
+	errorMessage, stopLoop = IsArray(
+		IntArray,
+		[]Validator{},
+	)([]int{})
+
+	if errorMessage != nil || stopLoop {
+		t.Errorf(
+			`
+errorMessage, stopLoop = IsArray(
+	IntArray,
+	[]Validator{},
+)([]int{}) = %v, %t; expected: nil, false
+			`,
+			getArgs()...,
+		)
+	}
+
+	errorMessage, stopLoop = IsArray(
+		IntArray,
+		[]Validator{},
+		customErrorMessage,
+	)([]int{})
+
+	if errorMessage != nil || stopLoop {
+		t.Errorf(
+			`
+errorMessage, stopLoop = IsArray(
+	IntArray,
+	[]Validator{},
+	"error",
+)([]int{}) = %v, %t; expected: nil, false
 			`,
 			getArgs()...,
 		)
