f78575a7fcdc6baaf8a2ba31719e66802fdadcf9
dariomatias-dev <matiasdario75@gmail.com>
2024-02-23 17:22:40 -0300

    fix: fixed "IsNullArray" validator
    - Fixed "IsNullArray" validator to adapt to the newly revamped "IsArray" validator.

--- is_null_array_validator.go
+++ is_null_array_validator.go
@@ -1,26 +1,34 @@
 package validators
 
 func IsNullArray(
+	typeOfValues string,
+	arraySettings Array,
 	fieldValidators []Validator,
 	errorMessage ...string,
 ) Validator {
-	// message := ""
-	// if len(errorMessage) != 0 {
-	// 	message = errorMessage[0]
-	// }
+	message := ""
+	if len(errorMessage) != 0 {
+		message = errorMessage[0]
+	}
 
 	return func(value interface{}) (*string, bool) {
 		if value == nil {
 			return nil, true
 		}
 
+		if message == "" {
+			return IsArray(
+				typeOfValues,
+				Array{},
+				fieldValidators,
+			)(value)
+		}
 
-		return nil, true
-
-		// if message == "" {
-		// 	return IsArray(fieldValidators)(value)
-		// }
-
-		// return IsArray(fieldValidators, message)(value)
+		return IsArray(
+			typeOfValues,
+			arraySettings,
+			fieldValidators,
+			message,
+		)(value)
 	}
 }
