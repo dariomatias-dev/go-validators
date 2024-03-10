42190f8ad624d6228c0a9ca4f8b4956ebb65e772
dariomatias-dev <matiasdario75@gmail.com>
2024-03-10 10:33:48 -0300

    test: tests created for the Validate function
    - Tests for the Validate function have been created.

---
+++ validate_test.go
@@ -0,0 +1,118 @@
+package validators
+
+import (
+	"testing"
+)
+
+func TestValidate(t *testing.T) {
+	// Test 1
+	test1 := func(
+		value any,
+		expected any,
+		condition func(errorsLen int) bool,
+	) {
+		errors := Validate(
+			value,
+			IsInt(),
+			Min(3),
+			Max(10),
+		)
+
+		if condition(len(*errors)) {
+			t.Errorf(`
+errors := Validate(
+	%v,
+	IsInt(),
+	Min(3),
+	Max(10),
+) = %v; expected: %v
+			`,
+				value,
+				*errors,
+				expected,
+			)
+		}
+	}
+
+	/// Error
+	test1(
+		"4",
+		"[error message(s)]",
+		func(errorsLen int) bool {
+			return errorsLen == 0
+		},
+	)
+
+	test1(
+		2,
+		"[error message(s)]",
+		func(errorsLen int) bool {
+			return errorsLen == 0
+		},
+	)
+
+	/// Success
+	test1(
+		4,
+		nil,
+		func(errorsLen int) bool {
+			return errorsLen != 0
+		},
+	)
+
+	// Test 2
+	test2 := func(
+		value any,
+		expected any,
+		condition func(errorsLen int) bool,
+	) {
+		errors := Validate(
+			value,
+			IsString(),
+			MinLength(3),
+			MaxLength(10),
+		)
+
+		if condition(len(*errors)) {
+			t.Errorf(`
+errors := Validate(
+	%v,
+	IsString(),
+	MinLength(3),
+	MaxLength(10),
+) = %v; expected: %v
+			`,
+				value,
+				*errors,
+				expected,
+			)
+		}
+	}
+
+	/// Error
+	test2(
+		nil,
+		"[error message(s)]",
+		func(errorsLen int) bool {
+			return errorsLen == 0
+		},
+	)
+
+	test2(
+		"Na",
+		"[error message(s)]",
+		func(errorsLen int) bool {
+			return errorsLen == 0
+		},
+	)
+
+	/// Success
+	test2(
+		"Name",
+		nil,
+		func(errorsLen int) bool {
+			return errorsLen != 0
+		},
+	)
+
+}
