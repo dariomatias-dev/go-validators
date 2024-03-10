bdf52d8cc0e1f3c2021fcf28af81896c85fb6efa
dariomatias-dev <matiasdario75@gmail.com>
2024-03-10 15:09:47 -0300

    doc: documented validators IsBool and IsNullBool
    - Documented validators IsBool and IsNullBool.

--- is_bool_validator.go
+++ is_bool_validator.go
@@ -2,6 +2,23 @@ package validators
 
 import "fmt"
 
+// Checks if the value is a boolean.
+//
+// Configuration parameters:
+//   - errorMessage (string): custom error message (optional)
+//
+// Input value (any): Value to be validated
+//
+// Usage examples:
+//
+//	value1 := true
+//	v.IsBool()(value1) // Output: nil, false
+//
+//	value2 := nil
+//	v.IsBool()(value2) // Output: [error message], true
+//
+//	value3 := 0
+//	v.IsBool()(value3) // Output: [error message], true
 func IsBool(
 	errorMessage ...string,
 ) Validator {
--- is_null_bool_validator.go
+++ is_null_bool_validator.go
@@ -2,6 +2,23 @@ package validators
 
 import "fmt"
 
+// Checks if the value is a boolean or null.
+//
+// Configuration parameters:
+//   - errorMessage (string): custom error message (optional)
+//
+// Input value (any): Value to be validated
+//
+// Usage examples:
+//
+//	value1 := true
+//	v.IsNullBool()(value1) // Output: nil, false
+//
+//	value2 := nil
+//	v.IsNullBool()(value2) // Output: nil, true
+//
+//	value3 := 0
+//	v.IsNullBool()(value3) // Output: [error message], true
 func IsNullBool(
 	errorMessage ...string,
 ) Validator {
