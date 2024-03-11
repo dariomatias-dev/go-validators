e797ee49a6c1ec6f9f22f56a892a9a52b6334755
dariomatias-dev <matiasdario75@gmail.com>
2024-03-11 16:21:10 -0300

    doc: documented validators IsAlpha and IsAlphaSpace
    - Documented validators IsAlpha and IsAlphaSpace.

--- is_alpha_space_validator.go
+++ is_alpha_space_validator.go
@@ -6,9 +6,26 @@ import (
 )
 
 var (
-	alphaSpaceRegex = regexp.MustCompile(`^[a-zA-ZÀ-ÖØ-öø-ÿ ]+$`)
+	alphaSpaceRegex = regexp.MustCompile(`^[a-zA-ZÀ-ÖØ-öø-ÿ\s]+$`)
 )
 
+// Checks if the value contains only letters and spaces.
+//
+// Configuration parameters:
+//   - errorMessage (string): custom error message (optional)
+//
+// Input value (string): value to be validated
+//
+// Usage examples:
+//
+//	value := "abcABC"
+//	v.IsAlphaSpace()(value) // Output: nil, false
+//
+//	value = "abcABC "
+//	v.IsAlphaSpace()(value) // Output: nil, false
+//
+//	value = "abcABC0123!@"
+//	v.IsAlphaSpace()(value) // Output: [error message], false
 func IsAlphaSpace(
 	errorMessage ...string,
 ) Validator {
--- is_alpha_validator.go
+++ is_alpha_validator.go
@@ -6,6 +6,23 @@ var (
 	alphaRegex = regexp.MustCompile(`^[a-zA-ZÀ-ÖØ-öø-ÿ]+$`)
 )
 
+// Checks if the value contains only letters.
+//
+// Configuration parameters:
+//   - errorMessage (string): custom error message (optional)
+//
+// Input value (string): value to be validated
+//
+// Usage examples:
+//
+//	value := "abcABC"
+//	v.IsAlpha()(value) // Output: nil, false
+//
+//	value = "abcABC "
+//	v.IsAlpha()(value) // Output: [error message], false
+//
+//	value = "abcABC0123!@"
+//	v.IsAlpha()(value) // Output: [error message], false
 func IsAlpha(
 	errorMessage ...string,
 ) Validator {
