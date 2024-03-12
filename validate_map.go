0a0532d560d77086165bd4fa55bbfeeaa91fc5a6
dariomatias-dev <matiasdario75@gmail.com>
2024-03-12 08:12:45 -0300

    doc: ValidateMap function documented
    - ValidateMap function documented.

--- validate_map.go
+++ validate_map.go
@@ -1,5 +1,40 @@
 package validators
 
+// Applies all validations to the respective fields
+//
+// Configuration parameters:
+//   - fieldsValidations (Validators): validators for each field
+//
+// Input value (map[string]any): Map to be validated
+//
+// Usage examples:
+//
+//	data := map[string]interface{}{
+//		"name":  "Name",
+//		"age":   18,
+//		"email": "emailexample@gmail.com",
+//	}
+//
+//	validations := v.Validators{
+//		"name": []v.Validator{
+//			v.IsString(),
+//			v.MinLength(3),
+//			v.MaxLength(20),
+//		},
+//		"age": []v.Validator{
+//			v.IsInt(),
+//			v.Min(18),
+//			v.Max(100),
+//		},
+//		"email": []v.Validator{
+//			v.Email(),
+//		},
+//	}
+//
+// v.ValidateMap(validations)(data) // Output: nil
+//
+// data["name"] = "Na"
+// v.ValidateMap(validations)(data) // Output: { [error messages] }
 func ValidateMap(
 	fieldsValidations Validators,
 ) func(data map[string]interface{}) *map[string][]string {
