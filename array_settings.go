86bc186ed99a15807df890abcddbc0e93f641b94
dariomatias-dev <matiasdario75@gmail.com>
2024-03-13 07:32:28 -0300

    doc: documented types Validators and Validator and Array struct for array validations
    - Added documentation for types Validators and Validator.
    - Added documentation for Array struct, configuring array validations for IsArray and IsNullArray.

--- array_settings.go
+++ array_settings.go
@@ -1,10 +1,22 @@
 package validators
 
+// Package validators provides configurations for the IsArray and IsNullArray validators.
 type Array struct {
-	AllowEmpty             bool
+	// AllowEmpty indicates whether the field can be empty or not.
+	AllowEmpty bool
+
+	// AllowEmptyErrorMessage is a custom error message to be displayed when the field is empty and not allowed.
 	AllowEmptyErrorMessage string
-	MinLength              int
-	MinLengthErrorMessage  string
-	MaxLength              int
-	MaxLengthErrorMessage  string
+
+	// MinLength specifies the minimum number of elements in the array.
+	MinLength int
+
+	// MinLengthErrorMessage is a custom error message to be displayed when the array has fewer elements than allowed.
+	MinLengthErrorMessage string
+
+	// MaxLength specifies the maximum number of elements in the array.
+	MaxLength int
+
+	// MaxLengthErrorMessage is a custom error message to be displayed when the array has more elements than allowed.
+	MaxLengthErrorMessage string
 }
