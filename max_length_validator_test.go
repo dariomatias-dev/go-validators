5ea99553c4f6ad2e7af7a5a5613c7b569988f25d
dariomatias-dev <matiasdario75@gmail.com>
2024-03-13 19:04:23 -0300

    test: improved organization of MinLength and MaxLength validators tests and made corrections
    - Enhanced organization of tests for MinLength and MaxLength validators.
    - Fixed absence of verification of the error message returned when the values are invalid when providing a custom error message.

--- max_length_validator_test.go
+++ max_length_validator_test.go
@@ -4,23 +4,30 @@ import "testing"
 
 func TestMaxLength(t *testing.T) {
 	customErrorMessage := "error"
+
+	/// - Successes
+	// Test 1
+	errorMessage, stopLoop = MaxLength(5)("aA")
+	if errorMessage != nil || stopLoop {
+		t.Errorf("MaxLength(5)(\"aA\") = %v, %t; expected: nil, false", getArgs()...)
+	}
+
+	// Test 2
+	errorMessage, stopLoop = MaxLength(5, customErrorMessage)("aA")
+	if errorMessage != nil || stopLoop {
+		t.Errorf("MaxLength(5, \"error\")(\"aA\") = %v, %t; expected: nil, false", getArgs()...)
+	}
+
+	/// - Errors
+	// Test 1
 	errorMessage, stopLoop = MaxLength(5)("aAbBcC")
 	if errorMessage == nil || stopLoop {
 		t.Errorf("MaxLength(5)(\"aAbBcC\") = %v, %t; expected: \"[error message]\", false", getArgs()...)
 	}
 
+	// Test 2
 	errorMessage, stopLoop = MaxLength(5, customErrorMessage)("aAbBcC")
-	if errorMessage == nil || stopLoop {
+	if errorMessage == nil || *errorMessage != customErrorMessage || stopLoop {
 		t.Errorf("MaxLength(5, \"error\")(\"aAbBcC\") = %v, %t; expected: \"error\", false", getArgs()...)
-	}
-
-	errorMessage, stopLoop = MaxLength(5)("aA")
-	if errorMessage != nil || stopLoop {
-		t.Errorf("MaxLength(5)(\"aA\") = %v, %t; expected: \"[error message]\", false", getArgs()...)
-	}
-
-	errorMessage, stopLoop = MaxLength(5, customErrorMessage)("aA")
-	if errorMessage != nil || stopLoop {
-		t.Errorf("MaxLength(5, \"error\")(\"aA\") = %v, %t; expected: \"error\", false", getArgs()...)
 	}
 }
--- min_length_validator_test.go
+++ min_length_validator_test.go
@@ -4,33 +4,42 @@ import "testing"
 
 func TestMinLength(t *testing.T) {
 	customErrorMessage := "error"
-	errorMessage, stopLoop = MinLength(5)("")
-	if errorMessage == nil || stopLoop {
-		t.Errorf("MinLength(5)(\"\") = %v, %t; expected: \"[error message]\", false", getArgs()...)
-	}
 
-	errorMessage, stopLoop = MinLength(5, customErrorMessage)("")
-	if errorMessage == nil || stopLoop {
-		t.Errorf("MinLength(5, \"error\")(\"\") = %v, %t; expected: \"error\", false", getArgs()...)
-	}
-
-	errorMessage, stopLoop = MinLength(5)("aA")
-	if errorMessage == nil || stopLoop {
-		t.Errorf("MinLength(5)(\"aA\") = %v, %t; expected: \"[error message]\", false", getArgs()...)
-	}
-
-	errorMessage, stopLoop = MinLength(5, customErrorMessage)("aA")
-	if errorMessage == nil || stopLoop {
-		t.Errorf("MinLength(5, \"error\")(\"aA\") = %v, %t; expected: \"error\", false", getArgs()...)
-	}
-
+	/// - Successes
+	// Test 1
 	errorMessage, stopLoop = MinLength(5)("aAbBcC")
 	if errorMessage != nil || stopLoop {
 		t.Errorf("MinLength(5)(\"aAbBcC\") = %v, %t; expected: nil, false", getArgs()...)
 	}
 
+	// Test 2
 	errorMessage, stopLoop = MinLength(5, customErrorMessage)("aAbBcC")
 	if errorMessage != nil || stopLoop {
 		t.Errorf("MinLength(5, \"error\")(\"aAbBcC\") = %v, %t; expected: nil, false", getArgs()...)
+	}
+
+	/// - Errors
+	// Test 1
+	errorMessage, stopLoop = MinLength(5)("")
+	if errorMessage == nil || stopLoop {
+		t.Errorf("MinLength(5)(\"\") = %v, %t; expected: \"[error message]\", false", getArgs()...)
+	}
+
+	// Test 2
+	errorMessage, stopLoop = MinLength(5, customErrorMessage)("")
+	if errorMessage == nil || *errorMessage != customErrorMessage || stopLoop {
+		t.Errorf("MinLength(5, \"error\")(\"\") = %v, %t; expected: \"error\", false", getArgs()...)
+	}
+
+	// Test 3
+	errorMessage, stopLoop = MinLength(5)("aA")
+	if errorMessage == nil || stopLoop {
+		t.Errorf("MinLength(5)(\"aA\") = %v, %t; expected: \"[error message]\", false", getArgs()...)
+	}
+
+	// Test 4
+	errorMessage, stopLoop = MinLength(5, customErrorMessage)("aA")
+	if errorMessage == nil || *errorMessage != customErrorMessage || stopLoop {
+		t.Errorf("MinLength(5, \"error\")(\"aA\") = %v, %t; expected: \"error\", false", getArgs()...)
 	}
 }
