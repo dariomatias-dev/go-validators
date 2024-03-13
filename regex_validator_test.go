273ba901e559b6a35df399d24248acce432f6476
dariomatias-dev <matiasdario75@gmail.com>
2024-03-13 19:22:26 -0300

    test: improved organization of URL and Regex validators tests
    - Enhanced organization of tests for URL and Regex validators.

--- regex_validator_test.go
+++ regex_validator_test.go
@@ -4,21 +4,10 @@ import "testing"
 
 func TestRegex(t *testing.T) {
 	numbersPattern := "^[0-9]+$"
-	errorMessage, stopLoop = Regex(
-		numbersPattern,
-		"Invalid value",
-	)("aA01")
-	if errorMessage == nil || stopLoop {
-		t.Errorf(`
-Regex(
-	"^[0-9]+$",
-	"Invalid value",
-)("aA01") = %s, %t; expected: "[error message]", false
-		`,
-			getArgs()...,
-		)
-	}
+	lettersPattern := "^[a-zA-Z]+$"
 
+	/// - Successes
+	// Test 1
 	errorMessage, stopLoop = Regex(
 		numbersPattern,
 		"Invalid value",
@@ -34,22 +23,7 @@ Regex(
 		)
 	}
 
-	lettersPattern := "^[a-zA-Z]+$"
-	errorMessage, stopLoop = Regex(
-		lettersPattern,
-		"Invalid value",
-	)("aA01")
-	if errorMessage == nil || stopLoop {
-		t.Errorf(`
-Regex(
-	"^[a-zA-Z]+$",
-	"Invalid value",
-)("aA01") = %s, %t; expected: "[error message]", false
-		`,
-			getArgs()...,
-		)
-	}
-
+	// Test 2
 	lettersPattern = "^[a-zA-Z]+$"
 	errorMessage, stopLoop = Regex(
 		lettersPattern,
@@ -61,6 +35,39 @@ Regex(
 	"^[a-zA-Z]+$",
 	"Invalid value",
 )("aAbB") = %s, %t; expected: nil, false
+		`,
+			getArgs()...,
+		)
+	}
+
+	/// - Errors
+	// Test 1
+	errorMessage, stopLoop = Regex(
+		numbersPattern,
+		"Invalid value",
+	)("aA01")
+	if errorMessage == nil || stopLoop {
+		t.Errorf(`
+Regex(
+	"^[0-9]+$",
+	"Invalid value",
+)("aA01") = %s, %t; expected: "[error message]", false
+		`,
+			getArgs()...,
+		)
+	}
+
+	// Test 2
+	errorMessage, stopLoop = Regex(
+		lettersPattern,
+		"Invalid value",
+	)("aA01")
+	if errorMessage == nil || stopLoop {
+		t.Errorf(`
+Regex(
+	"^[a-zA-Z]+$",
+	"Invalid value",
+)("aA01") = %s, %t; expected: "[error message]", false
 		`,
 			getArgs()...,
 		)
