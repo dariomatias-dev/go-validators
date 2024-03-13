7a2ced30a4dd6a87c707c0b791d8f4328f70ec88
dariomatias-dev <matiasdario75@gmail.com>
2024-03-13 18:45:51 -0300

    test: improved organization of Min and Max validators tests and made corrections
    - Enhanced organization of tests for Min and Max validators.
    - Fixed absence of verification of the error message returned when the values are invalid when providing a custom error message.

--- max_validator_test.go
+++ max_validator_test.go
@@ -4,43 +4,54 @@ import "testing"
 
 func TestMax(t *testing.T) {
 	customErrorMessage := "error"
-	errorMessage, stopLoop = Max(1)(2)
-	if errorMessage == nil || stopLoop {
-		t.Errorf("Max(1)(2) = %v, %t; expected: \"[error message]\", false", getArgs()...)
-	}
 
-	errorMessage, stopLoop = Max(1, customErrorMessage)(2)
-	if errorMessage == nil || stopLoop {
-		t.Errorf("Max(1, \"error\")(2) = %v, %t; expected: \"error\", false", getArgs()...)
-	}
-
-	errorMessage, stopLoop = Max(1.1)(1.2)
-	if errorMessage == nil || stopLoop {
-		t.Errorf("Max(1.1)(1.2) = %v, %t; expected: \"[error message]\", false", getArgs()...)
-	}
-
-	errorMessage, stopLoop = Max(1.1, customErrorMessage)(1.2)
-	if errorMessage == nil || stopLoop {
-		t.Errorf("Max(1.1, \"error\")(1.2) = %v, %t; expected: \"error\", false", getArgs()...)
-	}
-
+	/// - Successes
+	// Test 1
 	errorMessage, stopLoop = Max(2)(1)
 	if errorMessage != nil || stopLoop {
 		t.Errorf("Max(2)(1) = %v, %t; expected: nil, false", getArgs()...)
 	}
 
+	// Test 2
 	errorMessage, stopLoop = Max(2, customErrorMessage)(1)
 	if errorMessage != nil || stopLoop {
 		t.Errorf("Max(2, \"error\")(1) = %v, %t; expected: nil, false", getArgs()...)
 	}
 
+	// Test 3
 	errorMessage, stopLoop = Max(2.1)(1.1)
 	if errorMessage != nil || stopLoop {
 		t.Errorf("Max(2.1)(1.1) = %v, %t; expected: nil, false", getArgs()...)
 	}
 
+	// Test 4
 	errorMessage, stopLoop = Max(2.1, customErrorMessage)(1.1)
 	if errorMessage != nil || stopLoop {
 		t.Errorf("Max(2.1, \"error\")(1.1) = %v, %t; expected: nil, false", getArgs()...)
+	}
+
+	/// - Errors
+	// Test 1
+	errorMessage, stopLoop = Max(1)(2)
+	if errorMessage == nil || stopLoop {
+		t.Errorf("Max(1)(2) = %v, %t; expected: \"[error message]\", false", getArgs()...)
+	}
+
+	// Test 2
+	errorMessage, stopLoop = Max(1, customErrorMessage)(2)
+	if errorMessage == nil || *errorMessage != customErrorMessage || stopLoop {
+		t.Errorf("Max(1, \"error\")(2) = %v, %t; expected: \"error\", false", getArgs()...)
+	}
+
+	// Test 3
+	errorMessage, stopLoop = Max(1.1)(1.2)
+	if errorMessage == nil || stopLoop {
+		t.Errorf("Max(1.1)(1.2) = %v, %t; expected: \"[error message]\", false", getArgs()...)
+	}
+
+	// Test 4
+	errorMessage, stopLoop = Max(1.1, customErrorMessage)(1.2)
+	if errorMessage == nil || *errorMessage != customErrorMessage || stopLoop {
+		t.Errorf("Max(1.1, \"error\")(1.2) = %v, %t; expected: \"error\", false", getArgs()...)
 	}
 }
--- min_validator_test.go
+++ min_validator_test.go
@@ -4,43 +4,54 @@ import "testing"
 
 func TestMin(t *testing.T) {
 	customErrorMessage := "error"
-	errorMessage, stopLoop = Min(1)(0)
-	if errorMessage == nil || stopLoop {
-		t.Errorf("Min(1)(0) = %v, %t; expected: \"[error message]\", false", getArgs()...)
-	}
 
-	errorMessage, stopLoop = Min(1, customErrorMessage)(0)
-	if errorMessage == nil || stopLoop {
-		t.Errorf("Min(1, \"error\")(0) = %v, %t; expected: \"error\", false", getArgs()...)
-	}
-
-	errorMessage, stopLoop = Min(1.2)(1.1)
-	if errorMessage == nil || stopLoop {
-		t.Errorf("Min(1.2)(1.1) = %v, %t; expected: \"[error message]\", false", getArgs()...)
-	}
-
-	errorMessage, stopLoop = Min(1.2, customErrorMessage)(1.1)
-	if errorMessage == nil || stopLoop {
-		t.Errorf("Min(1.2, \"error\")(1.1) = %v, %t; expected: \"error\", false", getArgs()...)
-	}
-
+	/// - Successes
+	// Test 1
 	errorMessage, stopLoop = Min(1)(2)
 	if errorMessage != nil || stopLoop {
 		t.Errorf("Min(1)(2) = %v, %t; expected: nil, false", getArgs()...)
 	}
 
+	// Test 2
 	errorMessage, stopLoop = Min(1, customErrorMessage)(2)
 	if errorMessage != nil || stopLoop {
 		t.Errorf("Min(1, \"error\")(2) = %v, %t; expected: nil, false", getArgs()...)
 	}
 
+	// Test 3
 	errorMessage, stopLoop = Min(1.1)(2.1)
 	if errorMessage != nil || stopLoop {
 		t.Errorf("Min(1.1)(2.1) = %v, %t; expected: nil, false", getArgs()...)
 	}
 
+	// Test 4
 	errorMessage, stopLoop = Min(1.1, customErrorMessage)(2.1)
 	if errorMessage != nil || stopLoop {
 		t.Errorf("Min(1.1, \"error\")(2.1) = %v, %t; expected: nil, false", getArgs()...)
+	}
+
+	/// - Errors
+	// Test 1
+	errorMessage, stopLoop = Min(1)(0)
+	if errorMessage == nil || stopLoop {
+		t.Errorf("Min(1)(0) = %v, %t; expected: \"[error message]\", false", getArgs()...)
+	}
+
+	// Test 2
+	errorMessage, stopLoop = Min(1, customErrorMessage)(0)
+	if errorMessage == nil || *errorMessage != customErrorMessage || stopLoop {
+		t.Errorf("Min(1, \"error\")(0) = %v, %t; expected: \"error\", false", getArgs()...)
+	}
+
+	// Test 3
+	errorMessage, stopLoop = Min(1.2)(1.1)
+	if errorMessage == nil || stopLoop {
+		t.Errorf("Min(1.2)(1.1) = %v, %t; expected: \"[error message]\", false", getArgs()...)
+	}
+
+	// Test 4
+	errorMessage, stopLoop = Min(1.2, customErrorMessage)(1.1)
+	if errorMessage == nil || *errorMessage != customErrorMessage || stopLoop {
+		t.Errorf("Min(1.2, \"error\")(1.1) = %v, %t; expected: \"error\", false", getArgs()...)
 	}
 }
