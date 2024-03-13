254d4156b7711d9aec44d8d16c46eb32eea85b65
dariomatias-dev <matiasdario75@gmail.com>
2024-03-13 07:54:25 -0300

    doc: IsNullArray validator documented
    - IsNullArray validator documented.

--- is_null_array_validator.go
+++ is_null_array_validator.go
@@ -1,5 +1,43 @@
 package validators
 
+// Checks if the value is a valid array or nil
+//
+// Configuration parameters:
+//   - typeOfValues (string): type of array values
+//   - arraySettings (Array): array settings
+//   - fieldValidators ([]Validator): validators that must be applied to each value in the array
+//   - errorMessage (string): custom error message (optional)
+//
+// Input value ([]any): value to be validated
+//
+// Usage examples:
+//
+//	value1 := []string{}
+//	IsArray(
+//		arraytype.String,
+//		Array{
+//			AllowEmpty: true,
+//		},
+//		[]Validator{},
+//	)(value1) // Output: nil, false
+//
+//	value2 := nil
+//	IsArray(
+//		arraytype.String,
+//		Array{
+//			MinLength: 3,
+//		},
+//		[]Validator{},
+//	)(value2) // Output: nil, false
+//
+//	value3 := []string{"a", "b"}
+//	IsArray(
+//		arraytype.String,
+//		Array{
+//			MinLength: 3,
+//		},
+//		[]Validator{},
+//	)(value3) // Output: [error message], true
 func IsNullArray(
 	typeOfValues string,
 	arraySettings Array,
