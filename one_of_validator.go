9926a4caa779e0561dc0480dd42587032c940e7d
dariomatias-dev <matiasdario75@gmail.com>
2024-03-18 20:04:49 -0300

    feat: OneOf validator updated to also check values of type int and float64
    - Updated OneOf validator to also perform validation on values of type int and float64, not just strings.
    - Validator tests updated to accommodate the new way of using it.

--- one_of_validator.go
+++ one_of_validator.go
@@ -1,41 +1,92 @@
 package validators
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+
+	arraytype "github.com/dariomatias-dev/go-validators/array_type"
+)
 
 // Checks if the value is within certain options.
 //
 // Configuration parameters:
-//   - options([]string): value options.
+//   - arrayType (string): type of option array values.
+//   - options ([]string | []int | []float64): value options.
 //   - errorMessage (string): custom error message (optional).
 //
-// Input value (string): value to be validated.
+// Input value (string | int | float64): value to be validated.
 //
 // Usage examples:
 //
 //  options := []string{"one", "two", "three"}
-//	value := "three"
-//	v.OneOf(options)(value) // Output: nil, false
+//  value := "three"
+//  v.OneOf(options)(value) // Output: nil, false
 //
-//	value = "four"
-//	v.OneOf(options)(value) // Output: [error message], false
-//	v.OneOf(options, "error")(value) // Output: "error", false
+//  value = "four"
+//  v.OneOf(options)(value) // Output: [error message], false
+//  v.OneOf(options, "error")(value) // Output: "error", false
 func OneOf(
-	options []string,
+	arrayType string,
+	options any,
 	errorMessage ...string,
 ) Validator {
-	message := "Value not found in options: " + strings.Join(options, " | ") + "."
+	message := ""
 	if len(errorMessage) != 0 {
 		message = errorMessage[0]
+	} else {
+		message = "Value not found in options: "
+		switch arrayType {
+		case arraytype.String:
+			message += strings.Join(options.([]string), " | ")
+		case arraytype.Int:
+			for i, option := range options.([]int) {
+				if i == 0 {
+					message += fmt.Sprintf("%d | ", option)
+				} else if len(options.([]int))-1 == i {
+					message += fmt.Sprintf("%d", option)
+				} else {
+					message += fmt.Sprintf("%d | ", option)
+				}
+			}
+		case arraytype.Float64:
+			for i, option := range options.([]float64) {
+				if i == 0 {
+					message += fmt.Sprintf("%f | ", option)
+				} else if len(options.([]float64))-1 == i {
+					message += fmt.Sprintf("%f", option)
+				} else {
+					message += fmt.Sprintf("%f | ", option)
+				}
+			}
+		}
+		message += "."
 	}
 
-	val := ""
-
 	return func(value interface{}) (*string, bool) {
-		val = value.(string)
+		switch arrayType {
+		case arraytype.String:
+			val := value.(string)
 
-		for _, option := range options {
-			if val == option {
-				return nil, false
+			for _, option := range options.([]string) {
+				if val == option {
+					return nil, false
+				}
+			}
+		case arraytype.Int:
+			val := value.(int)
+
+			for _, option := range options.([]int) {
+				if val == option {
+					return nil, false
+				}
+			}
+		case arraytype.Float64:
+			val := value.(float64)
+
+			for _, option := range options.([]float64) {
+				if val == option {
+					return nil, false
+				}
 			}
 		}
 
--- one_of_validator_test.go
+++ one_of_validator_test.go
@@ -1,57 +1,61 @@
 package validators
 
-import "testing"
+import (
+	"testing"
+
+	arraytype "github.com/dariomatias-dev/go-validators/array_type"
+)
 
 func TestOneOf(t *testing.T) {
-	options := []string{"one", "two", "three"}
+	stringOptions := []string{"one", "two", "three"}
 
 	/// - Successes
 	// Test 1
-	errorMessage, stopLoop = OneOf(options)("one")
+	errorMessage, stopLoop = OneOf(arraytype.String, stringOptions)("one")
 	if errorMessage != nil || stopLoop {
-		t.Errorf("OneOf([]string{\"one\", \"two\", \"three\"})(\"one\") = %v, %t; expected: nil, false", getArgs()...)
+		t.Errorf("OneOf(arraytype.String, []string{\"one\", \"two\", \"three\"})(\"one\") = %v, %t; expected: nil, false", getArgs()...)
 	}
 
 	// Test 2
-	errorMessage, stopLoop = OneOf(options, customErrorMessage)("one")
+	errorMessage, stopLoop = OneOf(arraytype.String, stringOptions, customErrorMessage)("one")
 	if errorMessage != nil || stopLoop {
-		t.Errorf("OneOf([]string{\"one\", \"two\", \"three\"})(\"one\") = %v, %t; expected: nil, false", getArgs()...)
+		t.Errorf("OneOf(arraytype.String, []string{\"one\", \"two\", \"three\"})(\"one\") = %v, %t; expected: nil, false", getArgs()...)
 	}
 
 	// Test 3
-	errorMessage, stopLoop = OneOf(options)("three")
+	errorMessage, stopLoop = OneOf(arraytype.String, stringOptions)("three")
 	if errorMessage != nil || stopLoop {
-		t.Errorf("OneOf([]string{\"one\", \"two\", \"three\"})(\"three\") = %v, %t; expected: nil, false", getArgs()...)
+		t.Errorf("OneOf(arraytype.String, []string{\"one\", \"two\", \"three\"})(\"three\") = %v, %t; expected: nil, false", getArgs()...)
 	}
 
 	// Test 4
-	errorMessage, stopLoop = OneOf(options, customErrorMessage)("three")
+	errorMessage, stopLoop = OneOf(arraytype.String, stringOptions, customErrorMessage)("three")
 	if errorMessage != nil || stopLoop {
-		t.Errorf("OneOf([]string{\"one\", \"two\", \"three\"})(\"three\") = %v, %t; expected: nil, false", getArgs()...)
+		t.Errorf("OneOf(arraytype.String, []string{\"one\", \"two\", \"three\"})(\"three\") = %v, %t; expected: nil, false", getArgs()...)
 	}
 
 	/// - Errors
 	// Test 1
-	errorMessage, stopLoop = OneOf(options)("four")
+	errorMessage, stopLoop = OneOf(arraytype.String, stringOptions)("four")
 	if errorMessage == nil || stopLoop {
-		t.Errorf("OneOf([]string{\"one\", \"two\", \"three\"})(\"four\") = %v, %t; expected: [error message], false", getArgs()...)
+		t.Errorf("OneOf(arraytype.String, []string{\"one\", \"two\", \"three\"})(\"four\") = %v, %t; expected: [error message], false", getArgs()...)
 	}
 
 	// Test 2
-	errorMessage, stopLoop = OneOf(options, customErrorMessage)("four")
+	errorMessage, stopLoop = OneOf(arraytype.String, stringOptions, customErrorMessage)("four")
 	if errorMessage == nil || *errorMessage != customErrorMessage || stopLoop {
-		t.Errorf("OneOf([]string{\"one\", \"two\", \"three\"})(\"four\") = %v, %t; expected: \"error\"], false", getArgs()...)
+		t.Errorf("OneOf(arraytype.String, []string{\"one\", \"two\", \"three\"})(\"four\") = %v, %t; expected: \"error\"], false", getArgs()...)
 	}
 
 	// Test 3
-	errorMessage, stopLoop = OneOf(options)("five")
+	errorMessage, stopLoop = OneOf(arraytype.String, stringOptions)("five")
 	if errorMessage == nil || stopLoop {
-		t.Errorf("OneOf([]string{\"one\", \"two\", \"three\"})(\"five\") = %v, %t; expected: [error message], false", getArgs()...)
+		t.Errorf("OneOf(arraytype.String, []string{\"one\", \"two\", \"three\"})(\"five\") = %v, %t; expected: [error message], false", getArgs()...)
 	}
 
 	// Test 4
-	errorMessage, stopLoop = OneOf(options, customErrorMessage)("five")
+	errorMessage, stopLoop = OneOf(arraytype.String, stringOptions, customErrorMessage)("five")
 	if errorMessage == nil || *errorMessage != customErrorMessage || stopLoop {
-		t.Errorf("OneOf([]string{\"one\", \"two\", \"three\"})(\"five\") = %v, %t; expected: \"error\", false", getArgs()...)
+		t.Errorf("OneOf(arraytype.String, []string{\"one\", \"two\", \"three\"})(\"five\") = %v, %t; expected: \"error\", false", getArgs()...)
 	}
 }
