06018cea79ee85c3ab8f927dc7b27bd4619bd44d
dariomatias-dev <matiasdario75@gmail.com>
2024-03-18 14:47:25 -0300

    feat: EndsWith Validator created and documented
    - Validator that checks if a value ends with a specific sequence of characters created and documented.

---
+++ ends_with_validator.go
@@ -0,0 +1,40 @@
+package validators
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Checks whether the value ends with a given string.
+//
+// Configuration parameters:
+//   - endsWith(string): character sequence that the value must start with
+//   - errorMessage (string): custom error message (optional)
+//
+// Input value (string): value to be validated
+//
+// Usage examples:
+//
+//	value := "message"
+//	v.EndsWith("age")(value) // Output: nil, false
+//
+//	value := "send message"
+//	v.EndsWith("end")(value) // Output: [error message], false
+//	v.EndsWith("end", "error")(value) // Output: "error", false
+func EndsWith(
+	endsWith string,
+	errorMessages ...string,
+) Validator {
+	message := fmt.Sprintf("The value must end with: %s", endsWith)
+	if len(errorMessages) != 0 {
+		message = errorMessages[0]
+	}
+
+	return func(value interface{}) (*string, bool) {
+		if !strings.HasSuffix(value.(string), endsWith) {
+			return &message, false
+		}
+
+		return nil, false
+	}
+}
