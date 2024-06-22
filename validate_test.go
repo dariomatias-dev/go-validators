c6d77f134dfded815459ea2bb1c80b45811ecbbc
dariomatias-dev <matiasdario75@gmail.com>
2024-06-22 12:55:19 -0300

    feat: created success tests for the remaining validators and organization
    - Success tests for the last validators: IsArray, IsNullArray, Password, and Url.
    - Organized the JSON fields of the variable values in alphabetical order.

--- validate_test.go
+++ validate_test.go
@@ -12,9 +12,12 @@ type FieldValidations struct {
 	IsAlpha        string   `json:"isAlpha"        validates:"required;isString;isAlpha"`
 	IsAlphaSpace1  string   `json:"isAlphaSpace1"  validates:"isString;isAlphaSpace"`
 	IsAlphaSpace2  string   `json:"isAlphaSpace2"  validates:"required;isString;isAlphaSpace"`
+	IsArray        []string `json:"isArray"        validates:"required;minLength=1;maxLength=2;isArray;isString"`
 	IsBoolean      bool     `json:"isBoolean"      validates:"required;isBool"`
 	IsFloat        float64  `json:"isFloat"        validates:"required;isFloat;min=2;max=5"`
 	IsInt          int      `json:"isInt"          validates:"required;isInt;min=2;max=5"`
+	IsNullArray1   []string `json:"isNullArray1"   validates:"minLength=1;maxLength=2;isNullArray;isString"`
+	IsNullArray2   []string `json:"isNullArray2"   validates:"minLength=1;maxLength=2;isNullArray;isString"`
 	IsNullBoolean1 *bool    `json:"isNullBoolean1" validates:"isNullBool"`
 	IsNullBoolean2 *bool    `json:"isNullBoolean2" validates:"isNullBool"`
 	IsNullFloat1   *float64 `json:"isNullFloat1"   validates:"isNullFloat;min=2;max=5"`
@@ -29,33 +32,39 @@ type FieldValidations struct {
 	IsNumber1      float64  `json:"isNumber1"      validates:"required;isNumber;min=2;max=5"`
 	IsNumber2      float64  `json:"isNumber2"      validates:"required;isNumber;min=2;max=5"`
 	IsString       string   `json:"isString"       validates:"required;isString;minLength=2;maxLength=5"`
+	Password       string   `json:"password"       validates:"required;password"`
 	Regex          string   `json:"regex"          validates:"required;isString;regex=^[0-9]+$"`
 	StartsWith     string   `json:"startsWith"     validates:"required;isString;startsWith=na"`
 	StartsNotWith  string   `json:"startsNotWith"  validates:"required;isString;startsNotWith=ne"`
+	Url            string   `json:"url"            validates:"required;url"`
 }
 
 const values = `{
-	"email": "emailexample@gmail.com",
-	"endsWith": "name",
-	"endsNotWith": "name",
-	"isAlpha": "abcDEFáÉ",
-	"isAlphaSpace1": "abcDEFáÉ",
-	"isAlphaSpace2": "abcDEFáÉ ",
-	"isBoolean": true,
-	"isFloat": 3.3,
-	"isInt": 3,
+	"email":          "emailexample@gmail.com",
+	"endsWith":       "name",
+	"endsNotWith":    "name",
+	"isAlpha":        "abcDEFáÉ",
+	"isAlphaSpace1":  "abcDEFáÉ",
+	"isAlphaSpace2":  "abcDEFáÉ ",
+	"isArray":        ["name"],
+	"isBoolean":      true,
+	"isFloat":        3.3,
+	"isInt":          3,
+	"isNullArray2":   ["name"],
 	"isNullBoolean2": true,
-	"isNullFloat2": 3.3,
-	"isNullInt2": 3,
-	"isNullNumber2": 3,
-	"isNullNumber3": 3.3,
-	"isNullString2": "name",
-	"isNumber1": 3,
-	"isNumber2": 3.3,
-	"isString": "name",
-	"regex": "0123",
-	"startsWith": "name",
-	"startsNotWith": "name"
+	"isNullFloat2":   3.3,
+	"isNullInt2":     3,
+	"isNullNumber2":  3,
+	"isNullNumber3":  3.3,
+	"isNullString2":  "name",
+	"isNumber1":      3,
+	"isNumber2":      3.3,
+	"isString":       "name",
+	"password":       "aA1!",
+	"regex":          "0123",
+	"startsWith":     "name",
+	"startsNotWith":  "name",
+	"url":            "https://example.com"
 }`
 
 func TestValidate(t *testing.T) {
