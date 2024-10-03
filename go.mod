c9199f6ad9bfb85aababb5adc0aad9f965b9cabf
dariomatias-dev <matiasdario75@gmail.com>
2024-10-03 20:04:20 -0300

    feat: added UUID validator
    - Installed UUID package.
    - Created UUID validator.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dariomatias-dev/go-validators
 
 go 1.22.3
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ uuid_validator.go
@@ -0,0 +1,46 @@
+package validators
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+// Checks if the value is a valid UUID.
+//
+// Configuration parameters:
+//   - version (int): UUID version.
+//   - errorMessage (string): custom error message.
+//
+// Input value (string): value to be validated.
+//
+// Usage examples:
+//
+//	value := "f47ac10b-58cc-4372-a567-0e02b2c3d479"
+//	v.Uuid(4)(value)          // Output: nil, false
+//
+//	v.Uuid(3)(value)          // Output: [error message], false
+//	v.Uuid(3, "error")(value) // Output: "error", false
+//
+//	value = "00"
+//	v.Uuid(3)(value)          // Output: [error message], false
+//	v.Uuid(3, "error")(value) // Output: "error", false
+func Uuid(
+	version int,
+	errorMessage ...string,
+) Validator {
+	message := fmt.Sprintf("Invalid version %d UUID", version)
+	if len(errorMessage) != 0 {
+		message = errorMessage[0]
+	}
+
+	return func(value any) (error, bool) {
+		uuidValue, err := uuid.Parse(value.(string))
+		if err != nil || uuidValue.Version() != uuid.Version(version) {
+			return errors.New(message), false
+		}
+
+		return nil, false
+	}
+}
